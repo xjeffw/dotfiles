d851414d34b0304bcd6b0aa78101039f7ee4524c
Jeff Workman <jeff.workman@protonmail.com>
2026-01-30 21:47:16 -0500

    emacs from git

--- modules/common/programs/emacs/emacs.nix
+++ modules/common/programs/emacs/emacs.nix
@@ -1,50 +1,66 @@
-{ config, lib, pkgs, inputs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 with lib;
 with lib.my;
 let
-  inherit (config) user host modules theme;
+  inherit (config)
+    user
+    host
+    modules
+    theme
+    ;
   inherit (host) darwin;
   inherit (pkgs) fetchpatch;
   cfg = modules.programs.emacs;
   pwd = "${host.config-dir}/modules/common/programs/emacs";
 
-  emacsPkgs = (epkgs:
-    with epkgs; [
+  emacsPkgs = (
+    epkgs: with epkgs; [
       vterm
       all-the-icons
       treesit-grammars.with-all-grammars
       mu4e
       mu4e-alert
-    ]);
-  patches-url =
-    "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30";
-  emacs-base = optimizePkg {
-    enable = host.optimize;
-    level = 4;
-    native = true;
-  } (if darwin then
-    pkgs.emacs30.overrideAttrs (old: {
-      patches = (old.patches or [ ]) ++ [
-        # Fix OS window role so that yabai can pick up emacs
-        # (fetchpatch {
-        #   url = "${patches-url}/fix-window-role.patch";
-        #   sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
-        # })
-        # Enable rounded window with no decoration
-        (fetchpatch {
-          url = "${patches-url}/round-undecorated-frame.patch";
-          sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
-        })
-      ];
-      configureFlags = (old.configureFlags or [ ])
-        ++ [ "LDFLAGS=-headerpad_max_install_names" ];
-    })
-  else if modules.desktop.enable then
-    pkgs.emacs30-pgtk # from nixpkgs
-    # pkgs.emacs-git-pgtk # from emacs-overlay
-  else
-    pkgs.emacs30-nox);
-in {
+    ]
+  );
+  patches-url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30";
+  emacs-base =
+    optimizePkg
+      {
+        enable = host.optimize;
+        level = 4;
+        native = true;
+      }
+      (
+        if darwin then
+          pkgs.emacs30.overrideAttrs (old: {
+            patches = (old.patches or [ ]) ++ [
+              # Fix OS window role so that yabai can pick up emacs
+              # (fetchpatch {
+              #   url = "${patches-url}/fix-window-role.patch";
+              #   sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+              # })
+              # Enable rounded window with no decoration
+              (fetchpatch {
+                url = "${patches-url}/round-undecorated-frame.patch";
+                sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
+              })
+            ];
+            configureFlags = (old.configureFlags or [ ]) ++ [ "LDFLAGS=-headerpad_max_install_names" ];
+          })
+        else if modules.desktop.enable then
+          # pkgs.emacs30-pgtk # from nixpkgs
+          pkgs.emacs-git-pgtk # from emacs-overlay
+        else
+          pkgs.emacs30-nox
+      );
+in
+{
   options = {
     modules.programs.emacs = {
       enable = mkBoolOpt true;
@@ -66,11 +82,18 @@ in {
       })
     ];
 
-    environment.systemPackages = with pkgs; [ emacs xsel xclip ];
+    environment.systemPackages = with pkgs; [
+      emacs
+      xsel
+      xclip
+    ];
 
-    home-manager.users.${user.name} = { config, pkgs, ... }:
-      let link = config.lib.file.mkOutOfStoreSymlink;
-      in {
+    home-manager.users.${user.name} =
+      { config, pkgs, ... }:
+      let
+        link = config.lib.file.mkOutOfStoreSymlink;
+      in
+      {
         home.sessionPath = [ "${config.xdg.configHome}/emacs/bin" ];
 
         home.sessionVariables = {
@@ -91,7 +114,13 @@ in {
           isync
           gnutls
           graphviz
-          (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
+          (aspellWithDicts (
+            dicts: with dicts; [
+              en
+              en-computers
+              en-science
+            ]
+          ))
         ];
 
         xdg.configFile = {
