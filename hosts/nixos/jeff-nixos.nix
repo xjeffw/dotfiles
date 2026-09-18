029380ded8efd6232ea7df6149cdc4a52a15cb2e
Jeff Workman <jeff.workman@protonmail.com>
2026-09-17 23:17:14 -0400

    use zen kernel

--- hosts/nixos/jeff-nixos.nix
+++ hosts/nixos/jeff-nixos.nix
@@ -184,8 +184,8 @@ in
       };
     };
 
-    boot.kernelPackages = pkgs.linuxPackages;
-    # boot.kernelPackages = pkgs.linuxPackages_zen;
+    # boot.kernelPackages = pkgs.linuxPackages;
+    boot.kernelPackages = pkgs.linuxPackages_zen;
     # boot.kernelPackages = pkgs.linuxPackages_latest;
     # boot.kernelPackages = pkgs.linuxPackages_6_6;
     # boot.kernelPackages = pkgs.linuxPackages_lqx;
